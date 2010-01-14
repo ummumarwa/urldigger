3589157d23b25bcc9895590dcb7355d2dac4d3f7
ecasbas <ecasbas@a3b43d86-f5ea-11de-8f43-4547254af6c6>
2010-01-14 12:11:27 +0000

    more options/functions added

--- trunk/urldigger.py
+++ trunk/urldigger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
-from optparse import OptionParser
+import os
+#from optparse import OptionParser
+import optparse
 
 # (check this library on run time)
 from xgoogle.search import GoogleSearch, SearchError
@@ -31,22 +33,69 @@ def google(termtosearch):
 	except SearchError, e:
 	  print "Search failed: %s" % e	
 
+def alexa(country, n):
+	if country == "ES":
+		alexaES("ES")
+	elif country == "EN":
+		if n >= 1:
+			alexaEN(n)
+		alexaEN(2)
 
+
+def alexaES(country):
+	alexa = "http://www.alexa.com/topsites/countries/ES"
+	# check if hrefs.sh script exists
+	comando = "./hrefs.sh " + alexa
+	os.system(comando + '|grep -v alexa')
+
+def alexaEN(num):
+	url ="\"http://www.alexa.com/topsites/global;\""
+	# n=2 -> 20 results
+	n = 1
+	while (n < num):
+		top100 = url + repr(n)
+		comando = "./hrefs.sh " + top100
+		os.system(comando + '|grep -v alexa') 
+		n+=1
+
+def alexaHOT():
+	alexa = "http://www.alexa.com/hoturls"
+	comando = "./hrefs.sh " + alexa
+	os.system(comando + '|grep -v alexa')
+
+	
 def main():
     usage = "usage: %prog [options] arg"
-    parser = OptionParser(usage)
-    parser.add_option("-g", "--goog", dest="term",
-                      help="get urls with search term=term from google. Default is 100")
-    parser.add_option("-u", "--ul", dest="ulimit",
-                      help="no limit in the number of search url gets from google. Default is 100")
-    parser.add_option("-a", "--alexa", dest="country",
-                      help="get urls from alexa top sites with country=ES,EN ")
-    parser.add_option("-f", "--file", dest="filename",
-                      help="read data from FILENAME")
-    parser.add_option("-v", "--verbose",
+    parser = optparse.OptionParser(usage)
+
+    # Commands
+    commands = optparse.OptionGroup(parser, "Commands")
+    commands.add_option("-H", "--hot", action="store_true",
+			help="get hot urls from alexa")
+    parser.add_option_group(commands)
+
+
+    # Options
+    options = optparse.OptionGroup(parser, "Options")
+    options.add_option("-a", "--alexa", dest="country", 
+			help="get urls from alexa top sites with selected country [EN, ES]")
+    options.add_option("-g", "--goog", dest="term",
+                      help="get urls with search term=term from google [default 100]")
+    options.add_option("-n", "--num", dest="number", type="int",
+                      help="specify number urls to get [default 20]")
+    options.add_option("-u", "--ul", dest="ulimit",
+                      action="store", help="no limit in the number of search url gets from google. Default is 100")
+    parser.add_option_group(options)
+    
+
+    # Output
+    output = optparse.OptionGroup(parser, "Output")
+    output.add_option("-v", "--verbose",
                       action="store_true", dest="verbose")
-    parser.add_option("-q", "--quiet",
+    output.add_option("-q", "--quiet",
                       action="store_false", dest="verbose")
+    parser.add_option_group(output)
+
     (options, args) = parser.parse_args()
 
     if len(args) == 1:
@@ -60,10 +109,18 @@ def main():
 
     if options.country:
 	if options.country == 'ES':
-		print "ES"
+		alexa("ES", 1)
 	elif options.country == 'EN':
-		print "EN"
+		if options.number:
+			alexa("EN", options.number)
+		else: alexa("EN", 1)
 	else: print 'country option: \'%s\' not valid' %options.country
+
+    if options.hot:
+	alexaHOT()
+
+    #if options.number:
+#	print "Number specified"
 
     if options.term:
 	termtosearch = options.term
