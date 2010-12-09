c0475cefcccd5ccfa699c028d310a2c1178e4edf
ecasbas <ecasbas@gmail.com>
2010-12-09 11:50:59 +0000

    License and desc of script

--- check-URL-MD5.py
+++ check-URL-MD5.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python
 
+
+# Distributed under MIT license:
+#
+# Permission is hereby granted, free of charge, to any person
+# Obtaining a copy of this software and associated documentation
+# Files (the "Software"), to deal in the Software without
+# Restriction, including without limitation the rights to use,
+# Copy, modify, merge, publish, distribute, sublicense, and/or sell
+# Copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# Conditions:
+#
+# The above copyright notice and this permission notice shall be
+# Included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+
+# 2010 - Emilio Casbas <ecasbas@gmail.com>
+#
+# Script para verificar si ha habido algun cambio en un dominio.
+# Verifica el md5 de la pagina monitorizada, si este no coincide
+# con el original, envia un correo avisando de cambios (legitimos o fraudulentos)
+# 
+# Valido solo para paginas estaticas que no se actualizan, sitios web
+# de escaparates, tiendas online, paginas personales, etc.
+#
+
 import smtplib
 import urllib2
 from hashlib import md5
